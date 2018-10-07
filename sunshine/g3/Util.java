0456f6a50fcbc422158f490da333d16dab491e94
Warren <wmowkm@gmail.com>
2018-10-07 16:27:13 -0400

    bugfixes

--- sunshine/g3/Util.java
+++ sunshine/g3/Util.java
@@ -7,6 +7,9 @@ import sunshine.sim.Point;
 
 
 public class Util {
+
+    private static Point origin = new Point(0.0, 0.0);
+
     public static Double distance(Point a, Point b)
     {
         return Math.sqrt(Math.pow(a.x - b.x, 2) + Math.pow(a.y - b.y, 2));
@@ -29,15 +32,26 @@ public class Util {
         return nearestPoint;
     }
 
+    public static List<Double> getWeightList(List<Point> pointList)
+    {
+        List<Double> weightList = new ArrayList<Double>();
+        for (Point p: pointList)
+        {
+            weightList.add(1.0);
+        }
+
+        return weightList;
+    }
+
     public static Point centroid(List<Point> pointList, List<Double> weightList)
     {
-        assert pointList.getSize() == weightList.getSize() : "Point list and weight list not of equal length.";
+        assert pointList.size() == weightList.size() : "Point list and weight list not of equal length.";
 
         Double x = 0.0;
         Double y = 0.0;
         Double n = 0.0;
 
-        for (int i = 0; pointList.getSize(); i++)
+        for (int i = 0; i < pointList.size(); i++)
         {
             Point p = pointList.get(i);
             Double w = weightList.get(i);
@@ -53,39 +67,44 @@ public class Util {
     public static Point centroidTrailer(List<Point> pointList)
     {
         List<Point> pointListOrigin = new ArrayList<Point>(pointList);
-        pointListOrigin.add(new Point(0.0, 0.0));
+        pointListOrigin.add(origin);
 
         List<Double> weightListOrigin;
-        for (Point p: pointList)
-        {
-            weightListOrigin.add(1.0);
-        }
+        weightListOrigin = getWeightList(pointList);
         weightListOrigin.add(2.5);
 
         Point centroidOrigin = centroid(pointListOrigin, weightListOrigin);
         return centroidOrigin;
     }
 
-    /*
+    
     public static Point weiszfeld(List<Point> pointList)
     {
-        Point y_prev = centroid(pointList);
-        Point y_next = new Point(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+        List<Double> weightList = getWeightList(pointList); 
+        Point med_prev = centroid(pointList, weightList);
+        Point med_next = new Point(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
 
-        Double improvement = distance(y_prev, y_next);
+        Double improvement = distance(med_prev, med_next);
 
         // TODO may need to adjust cutoff 
         while (improvement > 1)
         {
-            Point sum_weighted_x = 0;
+            Double weighted_x = 0.0;
+            Double weighted_y = 0.0;
+            
             Double scale = 0.0;
             for (Point x: pointList)
             {
-                Double raw = distance(x, y);
-                xy_dist += distance(x, y);
+                Double raw = distance(x, med_prev);
+                weighted_x += x.x / raw;
+                weighted_y += x.y / raw;
+                scale += 1 / raw;
             }
+
+            med_next = new Point(weighted_x / scale, weighted_y / scale);
+            improvement = distance(med_prev, med_next);
+            med_prev = med_next;
         }
-        return null;
+        return med_next;
     }
-    */
 }
