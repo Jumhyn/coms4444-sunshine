0bb366e508293893a739779255184f0117d3c276
Warren <wmowkm@gmail.com>
2018-10-07 14:55:32 -0400

    centroid function

--- sunshine/g3/Util.java
+++ sunshine/g3/Util.java
@@ -27,4 +27,41 @@ public class Util {
         }
         return nearestPoint;
     }
+
+    public static Point centroid(List<Point> pointList)
+    {
+        Double x = 0;
+        Double y = 0;
+        Integer n = 0;
+
+        for (Point p: pointList)
+        {
+            x += p.x;
+            y += p.y;
+            n += 1;
+        }
+
+        return new Point(x/n, y/n);
+    }
+
+    public static Point weiszfeld(List<Point> pointList)
+    {
+        Point y_prev = centroid(pointList);
+        Point y_next = new Point(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+
+        Double improvement = distance(y_prev, y_next);
+
+        // TODO may need to adjust cutoff 
+        while (improvement > 1)
+        {
+            Point sum_weighted_x = 0;
+            Double scale = 0.0;
+            for (Point x: pointList)
+            {
+                Double raw = distance(x, y);
+                xy_dist += distance(x, y);
+            }
+        }
+        return null;
+    }
 }
