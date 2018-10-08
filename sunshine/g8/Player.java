51226f6aa4e0a688d9da6aecdb9c8f20218cd7a8
Wanlin Xie <wanlinx@gmail.com>
2018-10-07 22:05:54 -0400

    comparator done, basic case of choosing furthest bale

--- sunshine/g8/Player.java
+++ sunshine/g8/Player.java
@@ -1,10 +1,14 @@
 package sunshine.g8;
 
+import java.lang.Math;
+
+
 import java.util.List;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 import java.util.HashMap;
+import java.util.Comparator;
 
 import sunshine.sim.Command;
 import sunshine.sim.Tractor;
@@ -19,23 +23,41 @@ public class Player implements sunshine.sim.Player {
     private Random rand;
     
     List<Point> bales;
+    //Point origin;
 
     public Player() {
         rand = new Random(seed);
     }
+    private double distance(Point p1, Point p2) {
+        return Math.sqrt(Math.pow(p1.x-p2.x,2)+Math.pow(p1.y-p2.y,2));
+
+    }
+    Comparator<Point> pointComparator = new Comparator<Point>() {
+        @Override
+        public int compare(Point p1, Point p2) {
+            Point origin = new Point(0.0,0.0);
+            double d1 = distance(origin, p1);
+            double d2 = distance(origin, p2);
+            if (d1 < d2) {
+                return -1;
+            } else if (d1 > d2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
     
     public void init(List<Point> bales, int n, double m, double t)
     {
+
+        Collections.sort(bales,pointComparator);
         this.bales = bales;
+
         //System.out.println(n); //30 - number of tractors
         //System.out.println(m); //500 - length of field
         //System.out.println(t); //10000 - time
 
-
-        // PREPROCESS FIELD
-        // System.out.println(bales.size()); //number of bales
-        //System.out.println("HI"+bales.get(0).x); //point object
-        //System.out.println("HI"+bales.get(0).y);
         int cellSize = 200; //TODO
 
 
@@ -43,87 +65,25 @@ public class Player implements sunshine.sim.Player {
     
     public Command getCommand(Tractor tractor)
     {
-        if (tractor.getHasBale())
-        {
-            if (tractor.getLocation().equals(new Point(0.0, 0.0))) // has bale, at barn
-            {
-                if (tractor.getAttachedTrailer() != null) 
-                { //has bale, at barn, with trailer
-                    return new Command(CommandType.DETATCH); 
-                } 
-                else 
-                { // has bale at barn, no trailer
-                    if(rand.nextDouble() > 0.2) 
-                    {
-                        return new Command(CommandType.UNLOAD);
-                    }
-                    else 
-                    {
-                        return new Command(CommandType.UNSTACK);
-                    }
-                }
+        if (tractor.getHasBale()) {
+            if (tractor.getLocation().equals(new Point(0.0, 0.0))) {
+                return new Command(CommandType.UNLOAD);
+            } else {
+                return Command.createMoveCommand(new Point(0.0, 0.0));
             }
-            else // has bale, not at barn
-            {
-                if (tractor.getAttachedTrailer() != null) // has trailer, detach or move to barn
-                { 
-                    //Trailer t = tractor.getAttachedTrailer();
-                    if (tractor.getAttachedTrailer().getNumBales() < 10) 
-                    {
-                        return new Command(CommandType.DETATCH); 
-                    } else {
-                        return Command.createMoveCommand(new Point(0.0, 0.0)); //move to barn
-                    }
-
-                } 
-                else 
-                { // no trailer, move to barn, stack to nearest, or attach to trailer
-                    double r = rand.nextDouble();
-                    if (r < 0.1) 
-                    {
-                        return Command.createMoveCommand(new Point(0.0, 0.0)); //move to barn
-                    }
-                    else if (r < 0.5) 
-                    {
-                        return new Command(CommandType.STACK); 
-                    }
-                    else 
-                    {
-                        return new Command(CommandType.ATTACH);
-                    }
+        } else { // no bale
+            if (tractor.getLocation().equals(new Point(0.0, 0.0))) {
+                if (tractor.getAttachedTrailer() == null) {
+                    Point p = bales.remove(bales.size()-1);
+                    return Command.createMoveCommand(p);
+                } else {
+                    return new Command(CommandType.DETATCH);
                 }
 
-            }
-        } 
-        else 
-        {
-            if (tractor.getLocation().equals(new Point(0.0, 0.0))) //at barn
-            {
-                if (rand.nextDouble() > 0.5)
-                {
-                    if (tractor.getAttachedTrailer() == null) // attach/detatch trailer half the time
-                    {
-                        return new Command(CommandType.ATTACH); 
-                    }
-                    else
-                    {
-                        return new Command(CommandType.DETATCH);
-                    }
-                }
-                else if (bales.size() > 0)
-                {
-                    Point p = bales.remove(rand.nextInt(bales.size())); // find a bale to remove
-                    return Command.createMoveCommand(p); //move to that point
-                }
-                else
-                {
-                    return null;
-                }
-            }
-            else //not at barn
-            {
+            } else {
                 return new Command(CommandType.LOAD);
             }
+
         }
     }
 }
